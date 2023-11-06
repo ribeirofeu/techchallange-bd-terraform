aa233b2b7d44a8999cd93de14750a66093f38b48
RicardoAlmeida7 <ricardosalmeida.7@gmail.com>
2023-11-06 20:11:46 -0300

    removendo recursos

--- variables.tf
+++ variables.tf
@@ -1,16 +1,16 @@
-variable "region" {
-  description = "AWS region."
-}
-
-variable "db_user_name" {
-  description = "AWS db user name."
-}
-
-variable "db_pwd" {
-  description = "AWS db password."
-}
-
-variable "db_name" {
-  description = "Database name."
-  default     = "techfood"
-}
+//variable "region" {
+//  description = "AWS region."
+//}
+//
+//variable "db_user_name" {
+//  description = "AWS db user name."
+//}
+//
+//variable "db_pwd" {
+//  description = "AWS db password."
+//}
+//
+//variable "db_name" {
+//  description = "Database name."
+//  default     = "techfood"
+//}
